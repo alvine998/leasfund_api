eb0faf236d5c061321b8e7e83d0e43c563252bc3
alvin <alvinecom2018@gmail.com>
2025-02-13 19:59:10 +0700

    --

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 4000);
 }
-
 bootstrap();
-export default server;
